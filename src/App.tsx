d3076985edded837b1d40f13d6d8a266cc168ed6
Narendra Singh <narendra.rocks2002@gmail.com>
2025-01-09 22:04:30 +0530

    SetUp Completed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-slate-400'>
+      <h1>Plan Scheduler</h1>
+      <Button>Comming Soon</Button>
+    </div>
+  )
+}
+
+export default App
